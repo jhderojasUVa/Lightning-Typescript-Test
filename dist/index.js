0f0e730fd326c79b989af388738b0eec30c73c8c
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2020-04-09 11:15:15 +0100

    First commit
    First commit!

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+import { Launch } from 'wpe-lightning-sdk';
+import App from './App.js';
+export default function () {
+    return Launch(App, ...arguments);
+}
---
+++ test/default.test.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+
+var app = require('../src/App');
+
+describe('Begin your test here!', () => {
+
+});
